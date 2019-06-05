61bc1e12800d51d9f0784aa09a89f4dcfe1cb852
Yurii Zhytskyi <sweet3color@gmail.com>
2019-06-05 15:30:24 +0300

    test validation presence User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User
   field :last_name, type: String
   field :birthday, type: Date
   field :address, type: String
+
+  validates :first_name, :last_name, :birthday, :address, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid user' do
+    user = User.new first_name: 'First name', last_name: 'Last name', birthday: Date.today, address: 'Address'
+    assert user.valid?
+  end
+
+  test 'invalid without first_name' do
+    user = User.new first_name: '', last_name: 'Last name', birthday: Date.today, address: 'Address'
+    assert_not user.valid?
+  end
+
+  test 'invalid without last_name' do
+    user = User.new first_name: 'First name', last_name: '', birthday: Date.today, address: 'Address'
+    assert_not user.valid?
+  end
+
+  test 'invalid without birthday' do
+    user = User.new first_name: '', last_name: 'Last name', birthday: nil, address: 'Address'
+    assert_not user.valid?
+  end
+
+  test 'invalid without address' do
+    user = User.new first_name: 'First name', last_name: 'Last name', birthday: Date.new, address: ''
+    assert_not user.valid?
+  end
 end
