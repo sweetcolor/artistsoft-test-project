0d0a57bad0863c518109aa2e931298ba8a9bd3d6
Yurii Zhytskyi <sweet3color@gmail.com>
2019-06-05 17:55:28 +0300

    increase seeds to 200

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,11 @@
 
 # TODO move generator to lib
 
-RECORDS_COUNT = 20
+RECORDS_COUNT = 200
 MIN_NAME_LEN = 4
 MAX_NAME_LEN = 8
-CITIES = 3
-STREETS_IN_CITY = 4
+CITIES = 4
+STREETS_IN_CITY = 6
 
 def rand_name_generator
   ('a'..'z').to_a.shuffle[1..rand(MIN_NAME_LEN..MAX_NAME_LEN)].join.capitalize
@@ -33,7 +33,7 @@ cities = Array.new(CITIES) { rand_name_generator }
 streets_in_cities = Hash[cities.collect{ |k| [k, Array.new(STREETS_IN_CITY) { rand_name_generator }] }]
 
 RECORDS_COUNT.times.each do |i|
-  User.create first_name: "User #{i}",
+  User.create first_name: "User #{i.to_s.rjust RECORDS_COUNT.to_s.length, '0'}",
            last_name: rand_name_generator,
            birthday: rand_date_generator,
            address: rand_address_generator(cities, streets_in_cities)
